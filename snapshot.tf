6040f516b24031c0cfe25a345a087238de29dd73
kovbasaad <119735583+kovbasaad@users.noreply.github.com>
2023-02-27 01:48:52 +1000

    Update snapshot.tf

--- snapshot.tf
+++ snapshot.tf
@@ -41,17 +41,17 @@ resource "yandex_compute_snapshot" "vm-7disk" {
 
 }
 
-resource "yandex_compute_snapshot_schedule" "wsnapschedule" {
-  name           = "wsnapshotschedule"
+resource "yandex_compute_snapshot_schedule" "snapschedule" {
+  name           = "snapshotschedule"
 
   schedule_policy {
-    expression = "0 0 * * SUN"
+    expression = "0 0 ? * *"
   }
 
   snapshot_count = 1
 
   snapshot_spec {
-      description = "weekly-snapshot"
+      description = "daily-snapshot"
   }
 
   disk_ids = ["${yandex_compute_instance.vm-1.boot_disk[0].disk_id}", "${yandex_compute_instance.vm-2.boot_disk[0].disk_id}", "${yandex_compute_instance.vm-3.boot_disk[0].disk_id}", "${yandex_compute_instance.vm-4.boot_disk[0].disk_id}", "${yandex_compute_instance.vm-5.boot_disk[0].disk_id}", "${yandex_compute_instance.vm-6.boot_disk[0].disk_id}", "${yandex_compute_instance.vm-7.boot_disk[0].disk_id}"]
